2e6e6a778eedd5fd6e67734fa79238df4e97714f
Michael Kim <micklepickle98@gmail.com>
2017-08-21 21:55:01 -0700

    Add weather, micro scripts

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import reducers from './reducers';
 
 import Storage from './storage';
 import App from './containers/app.jsx';
-import Constants from './constants';
+import Constants, {createFile} from './constants';
 
 // Storage.clear();
 Storage.load(newState => {
@@ -32,7 +32,7 @@ Storage.load(newState => {
     function receiveMessage(event) {
         console.log(event);
         if (event.origin + '/' === Constants.WAT_TERM_CONTENT_URL) {
-            const message = event.data;
+            var message = event.data;
             const parts = event.data.split("|");
             console.log(event.data);
             if (parts[0] === 'done') {
@@ -48,20 +48,102 @@ Storage.load(newState => {
                     value: parts[2]
                 });
             }
-            // else if (parts[0] === 'requestFile') {
-            //     var result = getFile(message.substring(message.indexOf("|") + 1));
-            //     var contents = "";
-            //     if (result !== false) {
-            //         contents = result[0].data;
-            //     }
-            //     event.source.postMessage("file|" + contents, event.origin);
-            // }
-            // else if (parts[0] === 'writeFile') {
-            //     message = message.substring(message.indexOf("|") + 1);
-            //     createOrModifyFileAtPath(parts[1],
-            //             message.substring(message.indexOf("|") + 1));
-            // }
+            else if (parts[0] === 'requestFile') {
+                var result = getFile(message.substring(message.indexOf('|') + 1), store.getState().wfs);
+                var contents = '';
+                if (result !== false) {
+                    contents = result[0].data;
+                }
+                event.source.postMessage('file|' + contents, event.origin);
+            }
+            else if (parts[0] === 'writeFile') {
+                message = message.substring(message.indexOf('|') + 1);
+                createOrModifyFileAtPath(parts[1],
+                        message.substring(message.indexOf('|') + 1), store.getState().wfs);
+            }
         }
     }
 
 });
+
+
+function getFile(path, currDir) {
+    var end = path.lastIndexOf("/");
+    if (end === -1) return false;
+
+    var parts = path.split("/");
+    var filename = parts[parts.length - 1];
+    var containingDirRes = getDirectory(path.substring(0, end), currDir);
+    if (containingDirRes === false) return false;
+
+    var containingPath = containingDirRes[1];
+    var containingDir = containingDirRes[0];
+    for (var j = 0; j < containingDir.data.length; j++) {
+        if (containingDir.data[j].type === Constants.FILE_TYPE &&
+            containingDir.data[j].name === filename) {
+            return [containingDir.data[j], containingPath + "/" + filename];
+        }
+    }
+    return false;
+}
+
+function getDirectory(directory, currDir) {
+    var parts = directory.split('/');
+    var dirStack = [currDir];
+    for (var i = 1; i < parts.length; i++) {
+        if (parts === "" || parts === ".") continue;
+        if (parts[i] === "..") {
+            currDir = dirStack[dirStack.length - 1];
+            if (dirStack.length != 1) {
+                dirStack.pop();
+            }
+            continue;
+        }
+        else if (parts[i] == "~") {
+            // Go Back to Root
+            while (dirStack.length != 1) {
+                dirStack.pop();
+            }
+            continue;
+        }
+        var found = -1;
+        for (var j = 0; j < currDir.data.length; j++) {
+            // Found it
+            if (currDir.data[j].name === parts[i] && currDir.data[j].type === Constants.DIR_TYPE) {
+                found = j;
+                break;
+            }
+        }
+        if (found != -1) {
+            dirStack.push(currDir.data[j])
+        }
+        else {
+            return false;
+        }
+        currDir = dirStack[dirStack.length - 1];
+    }
+    var path = "";
+    for (var i = 0; i < dirStack.length; i++) {
+        if (i != 0) path += "/";
+        path += dirStack[i].name;
+    }
+    return [currDir, path];
+}
+
+function createOrModifyFileAtPath(path, contents, wfs) {
+    var fileObject = getFile(path, wfs);
+    console.log(fileObject);
+    var parts = path.split("/");
+    if (fileObject == false) {
+        // Need to Create
+        var enclosingDir = getDirectory(path.substring(0, path.lastIndexOf("/")), wfs);
+        if (enclosingDir != false) {
+            var newFile = createFile(parts[parts.length - 1]);
+            newFile.data = contents;
+            enclosingDir[0].data.push(newFile);
+        }
+    }
+    else {
+        fileObject[0].data = contents;
+    }
+}
